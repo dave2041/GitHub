1556c7430e9e92def066d1d14c1cf2f677b81507
dave2041 <davidmstammers@gmail.com>
2014-11-14 10:38:27 +0000

    Config Pack Stuff

---
+++ ConfigurationPackageCreator/resource.h
@@ -0,0 +1,81 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Configuration Package Creator.rc
+//
+#define IDOK                            1
+#define IDCREATE                        1
+#define IDCANCEL                        2
+#define IDD_CONFIGURATIONPACKAGECREATOR_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDI_ICON1                       136
+#define IDI_ICON2                       137
+#define IDI_ICON3                       138
+#define IDI_ICON4                       139
+#define IDR_MENU1                       141
+#define IDC_EDIT1                       1000
+#define IDC_LOAD_EDIT                   1000
+#define IDC_EDIT_LOAD                   1000
+#define IDC_LOAD_BUTTON                 1001
+#define IDC_LOAD                        1001
+#define IDC_TREE1                       1002
+#define IDC_TREE                        1002
+#define IDC_RADIO_YES_HIDE_USER         1005
+#define IDC_RADIO_NO_HIDE_USER          1006
+#define IDC_RADIO_YES_HIDE_SERVER       1007
+#define IDC_RADIO_NO_HIDE_SERVER        1008
+#define IDC_RADIO_YES_READ_ONLY         1009
+#define IDC_CHECK_READ_ONLY_TERMINAL_YES 1009
+#define IDC_RADIO_READ_ONLY_TERMINAL_YES 1009
+#define IDC_RADIO_NO_READ_ONLY          1010
+#define IDC_RADIO_READ_ONLY_TERMINAL_NO 1010
+#define IDC_EDIT_VALUE                  1011
+#define IDC_RADIO_ENABLED               1012
+#define IDC_CHECK_NO_CHANGE_HIDE_USER   1013
+#define IDC_CHECK_NO_CHANGE_HIDE_SERVER 1014
+#define IDC_CHECK_NO_CHANGE_READ_ONLY   1015
+#define IDC_CHECK_READ_ONLY_TERMINAL_USE_DEFAULT 1015
+#define IDC_EDIT_PACKAGE                1016
+#define IDC_EDIT_VERSION                1017
+#define IDC_RADIO_DISABLED              1018
+#define IDC_CHECK_NO_CHANGE_VALUE       1019
+#define IDC_MIN                         1020
+#define IDC_MAX                         1021
+#define IDC_RADIO_READ_ONLY_SERVER_YES  1022
+#define IDC_CANCEL                      1023
+#define IDC_BUTTON_CANCEL               1023
+#define IDC_COMBO1                      1024
+#define IDC_VALUE_COMBO                 1024
+#define IDC_APPLY_BUTTON                1025
+#define IDC_BROWSE_EDIT                 1026
+#define IDC_BUTTON1                     1027
+#define IDC_STATIC_LOAD                 1028
+#define IDC_STATIC_PACKAGE              1029
+#define IDC_STATIC_VERSION              1030
+#define IDC_RADIO_READ_ONLY_SERVER_NO   1031
+#define IDC_CHECK_READ_ONLY_SERVER_USE_DEFAULT 1032
+#define IDC_BUTTON_SAVE_AS              1037
+#define IDC_BUTTON_SAVE                 1038
+#define IDC_BUTTON_RESET_ALL            1039
+#define IDC_RADIO_HIDE_GAMINGMACHINE    1040
+#define IDC_RADIO_READWRITE_GAMINGMACHINE 1041
+#define IDC_RADIO_READONLY_GAMINGMACHINE 1042
+#define IDC_RADIO_HIDE_MANAGEMENT       1043
+#define IDC_RADIO_READWRITE_MANAGEMENT  1044
+#define IDC_RADIO_READONLY_MANAGEMENT   1045
+#define IDC_RADIO_UNHIDE_GAMINGMACHINE  1046
+#define IDC_RADIO_UNHIDE_MANAGEMENT     1047
+#define ID_FILE_NEW32771                32771
+#define ID_FILE_OPEN32772               32772
+#define ID_FILE_SAVE32773               32773
+#define ID_FILE_EXIT                    32774
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        142
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1043
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
