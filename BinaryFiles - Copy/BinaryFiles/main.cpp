f435b60e8ee69c1a6d14ffdabf1cec99caadb336
dave2041 <davidmstammers@gmail.com>
2014-11-14 10:37:26 +0000

    BinaryFiles Update

---
+++ BinaryFiles - Copy/BinaryFiles/main.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string>
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+	int main()
+	{
+		int iResult = 0;
+		int desiredResult;
+		vector<int> vecInt;
+		vecInt.push_back(4);
+		vecInt.push_back(1);
+		vecInt.push_back(9);
+		vecInt.push_back(1);
+		vecInt.push_back(75);
+		vecInt.push_back(10);
+
+		while(iResult != desiredResult)
+		{
+			// Try math
+		
+		}
+		int ii = 0;
+		return 0;
+	}
