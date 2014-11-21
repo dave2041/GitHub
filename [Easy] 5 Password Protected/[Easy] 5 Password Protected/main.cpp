933e1c73792074279df28485c5d999b2ec773f9f
dave2041 <davidmstammers@gmail.com>
2014-11-21 19:12:47 +0000

    Work on Challenge 5
    Description

--- [Easy] 5 Password Protected/[Easy] 5 Password Protected/main.cpp
+++ [Easy] 5 Password Protected/[Easy] 5 Password Protected/main.cpp
@@ -11,16 +11,42 @@ int main()
 {
 	ifstream infile("passwords.txt");
 	string line;
-	string strUser, strPass;
+	string strUser, strPass, strUserInput, strPassInput = "";
 	map<string, string> mapUsers;
+	bool bAllowAccess = false;
 
-	while (getline(infile, line))
+	while (true)
 	{
-		strUser = strPass = "";
-		sscanf_s(line.c_str(), "%s %s", strUser, strPass);
-		mapUsers.insert(strUser, strPass);
-	};
+		bAllowAccess = false;
 
+		cout << "Please enter your username: " << endl;
+		cin >> strUserInput;
+		cout << "\nPlease enter a password" << endl;
+		cin >> strPassInput;
+
+		while (getline(infile, line))
+		{
+			strUser = strPass = "";
+			int commaPos = line.find(',');
+			strUser = line.substr(0, commaPos);
+			strPass = line.substr(commaPos + 1, line.length());
+
+			if (strUserInput == strUser && strPassInput == strPass)
+			{
+				bAllowAccess = true;
+				break;
+			}
+		}
+
+		if (bAllowAccess)
+		{
+			cout << "\n\n----------------------------------------------------" << endl;
+			cout << "Welcome to this useless program!" << endl;
+			cout << "----------------------------------------------------" << endl << endl;
+		}
+		else
+			cout << "No access for you!!!\n\n";
+	}
 	system("pause");
 	return 0;
 }
